9ce7ddf0cfda3939247364ab2b0a5d8d5baa0d79
Klem_6 <clemence.merou@gmail.com>
2025-03-16 13:33:27 +0100

    start extraits

--- script.js
+++ script.js
@@ -17,4 +17,32 @@ document.addEventListener('DOMContentLoaded', function () {
             });
         });
     });
+
+    // ****** Slider Videos
+    const CaroS = document.querySelector('.Carousel-slider');
+    const CaroSlider = new MicroSlider(CaroS, { indicators: true, indicatorText: '' });
+    const prevButton = document.getElementById('prev-slide');
+    const nextButton = document.getElementById('next-slide');
+
+    // Écouteurs d'événements pour avancer/reculer
+    prevButton.addEventListener('click', () => {
+        CaroSlider.prev();
+    });
+
+    nextButton.addEventListener('click', () => {
+        CaroSlider.next();
+    });
+
+    // Gestion du clic sur les slides (ouvrir un lien si nécessaire)
+    let slideLinks = document.querySelectorAll('.slider-item');
+    if (slideLinks.length > 0) {
+        slideLinks.forEach(el =>
+            el.addEventListener('click', e => {
+                e.preventDefault();
+                let href = el.dataset.href;
+                let target = el.dataset.target || '_self';
+                if (href !== '#') window.open(href, target);
+            })
+        );
+    }
 });
